8d9812a0765ae21cc0f9f05199f9d90a0c70318a
MED003BK <80395200+MED003BK@users.noreply.github.com>
2021-03-29 15:57:50 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+
+var countDownDate = new Date("May 14, 2021 11:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("count").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("count").innerHTML = "Finiched";
+    }
+}, 1000);
